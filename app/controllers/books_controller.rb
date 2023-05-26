8442ea0cbd661b5b268a464a681d33b52ee19bd4
Antonio Gorgulho <antonioSgorgulho@gmail.com>
2023-05-26 10:40:03 +0100

    Finish book form and display

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -3,7 +3,7 @@ class BooksController < ApplicationController
 
   # GET /books or /books.json
   def index
-    @books = Book.all
+    @books = Book.order(created_at: :desc)
   end
 
   # GET /books/1 or /books/1.json
@@ -25,7 +25,7 @@ class BooksController < ApplicationController
 
     respond_to do |format|
       if @book.save
-        format.html { redirect_to book_url(@book), notice: "Book was successfully created." }
+        format.html { redirect_to books_path(@book), notice: "Book was successfully created." }
         format.json { render :show, status: :created, location: @book }
       else
         format.html { render :new, status: :unprocessable_entity }
@@ -38,7 +38,7 @@ class BooksController < ApplicationController
   def update
     respond_to do |format|
       if @book.update(book_params)
-        format.html { redirect_to book_url(@book), notice: "Book was successfully updated." }
+        format.html { redirect_to books_path(@book), notice: "Book was successfully updated." }
         format.json { render :show, status: :ok, location: @book }
       else
         format.html { render :edit, status: :unprocessable_entity }
@@ -49,11 +49,14 @@ class BooksController < ApplicationController
 
   # DELETE /books/1 or /books/1.json
   def destroy
-    @book.destroy
+    if @book.destroy
+      operations = cable_car.remove(selector: dom_id(@book))
+                            .dispatch_event(name: 'submit:success')
 
-    respond_to do |format|
-      format.html { redirect_to books_url, notice: "Book was successfully destroyed." }
-      format.json { head :no_content }
+      render operations: operations
+    else
+      render operations: cable_car
+        .dispatch_event(name: 'submit:error')
     end
   end
 
--- app/models/book.rb
+++ app/models/book.rb
@@ -1,2 +1,4 @@
 class Book < ApplicationRecord
+  validates_presence_of :title, :author, :good_reads_score, :good_reads_url,
+    :good_reads_description, :cover_url, :publish_date
 end
